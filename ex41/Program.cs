e9d21249154453f112885665e2563413da91d4b7
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-07 23:58:09 +0700

    feat: add solution of task 41

---
+++ ex41/Program.cs
@@ -0,0 +1,28 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] fillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            result++;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите количество чисел");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = fillArray(length);
+Console.WriteLine($"Количество положительных чисел {CountPositiveNumbers(numbers)}");
